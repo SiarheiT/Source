5bfba60ec9462babfedf84a4933eb684345a00b8
Siarhei Tuzik <Siarhei_Tuzik@epam.com>
2022-12-28 18:09:23 +0300

    Add more fonts to EPAM menu script

--- EPAM_MenuPCF/EPAMMenuPCF/index.ts
+++ EPAM_MenuPCF/EPAMMenuPCF/index.ts
@@ -97,7 +97,7 @@ export class EPAMMenuPCF implements ComponentFramework.StandardControl<IInputs, 
             var link = document.createElement("link");
             link.id = "PowerFontUrl";
             link.rel = "stylesheet";
-            link.href = `https://fonts.googleapis.com/css?family=Source+Sans+Pro&display=swap`;
+            link.href = `https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,200;0,300;0,400;0,700;0,900;1,200;1,300;1,400;1,600;1,700;1,900&display=swap`;
         
             document.getElementsByTagName("head")[0].appendChild(link);
 
